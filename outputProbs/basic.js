80c57b809ac40f6f7c3275b72026559a8164d4a9
Sashank <sashankakkabattula@gmail.com>
2026-04-22 12:23:03 +0530

    output problems

--- outputProbs/basic.js
+++ outputProbs/basic.js
@@ -2,22 +2,36 @@
 var a = 10;
 function multiply() {
   var a; // Hoisted! It exists now, but has no value (undefined)
-  console.log(a); // logs undefined here!
+  //console.log(a); // logs undefined here!
   a = 20; // value is finally assigned here
-  console.log(a); // logs 20
+  //console.log(a); // logs 20
 }
 multiply();
-console.log(a); //10
+//console.log(a); //10
 
 // 2
 
 for (var i = 0; i < 3; i++) {
   setTimeout(() => {
-    console.log(i);
+    // console.log(i);
   }, 1000); // 3,3,3 -- all appearing at once after 1 second)
 }
 for (let i = 0; i < 3; i++) {
   setTimeout(() => {
-    console.log(i);
+    //console.log(i);
   }, 1000); // 0,1,2-- with a 1-second delay between each)
 }
+
+console.log(typeof NaN); // prints: number
+console.log(typeof value === "number" && isNaN(value)); // FALSE
+console.log(Number.isNaN(NaN)); // TRUE
+console.log({} + []); // [object Object]
+console.log(typeof {} + []); // object
+console.log({} === {}); //false
+console.log({} == {}); //false
+console.log([] == ![]); //true
+console.log({} == !{}); //false
+console.log(5 == "5"); // true
+console.log(5 === "5"); //false
+console.log(1 == true); //true
+console.log(1 === true); //false
